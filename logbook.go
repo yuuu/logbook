621c54c18342eb4e3e9285ba94d61439f6cfe20d
Yuhei Okazaki <okazaki@y-uuu.net>
2017-07-09 06:20:35 +0900

    test subcommands.

--- logbook.go
+++ logbook.go
@@ -1,1 +1,9 @@
 package main
+
+type Logbook struct {
+	name string
+}
+
+func LoadDiary(path string) (*Logbook, error) {
+	return &Logbook{"test"}, nil
+}
--- main.go
+++ main.go
@@ -5,45 +5,68 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
+	"time"
 
 	"github.com/google/subcommands"
 )
 
-type printCmd struct {
-	capitalize bool
+type keepCmd struct {
 }
 
-func (*printCmd) Name() string     { return "print" }
-func (*printCmd) Synopsis() string { return "Print args to stdout." }
-func (*printCmd) Usage() string {
-	return `print [-capitalize] <some text>:
-  Print args to stdout.
+func (*keepCmd) Name() string     { return "keep" }
+func (*keepCmd) Synopsis() string { return "Keep a diary." }
+func (*keepCmd) Usage() string {
+	return `keep:
+  Keep a diary
 `
 }
-
-func (p *printCmd) SetFlags(f *flag.FlagSet) {
-	f.BoolVar(&p.capitalize, "capitalize", false, "capitalize output")
+func (p *keepCmd) SetFlags(f *flag.FlagSet) {
 }
-
-func (p *printCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+func (p *keepCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 	for _, arg := range f.Args() {
-		if p.capitalize {
-			arg = strings.ToUpper(arg)
-		}
 		fmt.Printf("%s ", arg)
 	}
 	fmt.Println()
 	return subcommands.ExitSuccess
 }
 
+type entryCmd struct {
+	date string
+}
+
+func (*entryCmd) Name() string     { return "entry" }
+func (*entryCmd) Synopsis() string { return "Display an entry." }
+func (*entryCmd) Usage() string {
+	return `entry:
+  Display an entry.
+`
+}
+func (p *entryCmd) SetFlags(f *flag.FlagSet) {
+	f.StringVar(&p.date, "date", time.Now().Format("2006-01-02"), "date of entry")
+}
+func (p *entryCmd) Execute(_ context.Context, f *flag.FlagSet, argv ...interface{}) subcommands.ExitStatus {
+	var lgbk = (argv[0]).(*Logbook)
+
+	t, err := time.Parse("2006-01-02", p.date)
+	if err != nil {
+		fmt.Println(p.date + " is invalud date.")
+	} else {
+		fmt.Println(t.Format(lgbk.name + ": " + "2006-01-02"))
+	}
+
+	return subcommands.ExitSuccess
+}
+
 func main() {
+	lgbk, _ := LoadDiary("")
+
 	subcommands.Register(subcommands.HelpCommand(), "")
 	subcommands.Register(subcommands.FlagsCommand(), "")
 	subcommands.Register(subcommands.CommandsCommand(), "")
-	subcommands.Register(&printCmd{}, "")
+	subcommands.Register(&keepCmd{}, "")
+	subcommands.Register(&entryCmd{}, "")
 
 	flag.Parse()
 	ctx := context.Background()
-	os.Exit(int(subcommands.Execute(ctx)))
+	os.Exit(int(subcommands.Execute(ctx, lgbk)))
 }
